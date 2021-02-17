af686f02d87e41f44a5db1468cb5a0de54b6c4e6
presola <53655433+presola@users.noreply.github.com>
2021-02-17 03:34:30 -0400

    update to CORS

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from zoomParser import ZoomURL, ZoomParser
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4200",
+    "https://zoomurl-angular.herokuapp.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
